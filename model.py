2325f40492ea9e8c0030b35c91983dfb4a45005f
vanwtoanf <damtoan321@gmail.com>
2025-05-29 00:06:48 +0700

    ....

--- model.py
+++ model.py
@@ -1,32 +1,3 @@
-# import torch
-# import torch.nn as nn
-#
-# class MIMOEstimatorMLP(nn.Module):
-#     def __init__(self, input_dim=8, output_dim=8, hidden_dim=128):
-#         super().__init__()
-#         self.net = nn.Sequential(
-#             nn.Linear(input_dim, hidden_dim),
-#             nn.BatchNorm1d(hidden_dim),
-#             nn.ReLU(),
-#
-#             nn.Linear(hidden_dim, hidden_dim),
-#             nn.BatchNorm1d(hidden_dim),
-#             nn.ReLU(),
-#
-#             nn.Linear(hidden_dim, hidden_dim),
-#             nn.BatchNorm1d(hidden_dim),
-#             nn.ReLU(),
-#
-#             nn.Linear(hidden_dim, output_dim)
-#         )
-#
-#     def forward(self, x):
-#         return self.net(x)
-
-
-
-
-
 import torch.nn as nn
 import torch.nn.functional as F
 
